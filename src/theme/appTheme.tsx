9d54f2545950aa9dba12113b9466a9ee511b2327
DiegoEDG <1.5.dg.diego@gmail.com>
2022-06-23 14:04:04 -0500

    App Setup

---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles=StyleSheet.create({
+  globalAppStyles: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  globalTextStyle:{
+    color: 'white',
+    fontSize: 20
+  }
+})
